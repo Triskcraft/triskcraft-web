a95d9e4676ba9bc30e17cbbecbf613247c6107b1
Elí Maciel <tecno_eli@hotmail.com>
2026-03-06 00:15:04 -0600

    create blog route

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+export default async function BlogPage() {
+    return (
+        <div>
+            <h1>Blog</h1>
+            <p>
+                aqui iran las entradas publicadas con un fragmento del primer
+                parrafo y el autor
+            </p>
+        </div>
+    )
+}
